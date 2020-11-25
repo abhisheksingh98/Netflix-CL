6872b3204d013c0b8b47f43603a69dc03217a522
Abhishek Singh <abhishek.singh09@nagarro.com>
2020-11-25 21:03:53 +0530

    finished developing navbar

--- src/Nav.js
+++ src/Nav.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Nav.css'
 
 function Nav() {
-    const [show, handleshow] = useState(false)
+    const [show, handleshow] = useState(false);
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -16,7 +16,7 @@ function Nav() {
       }, []);
 
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav__black"}`}>
         <img
         className="nav__logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
